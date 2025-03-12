ac6e989bf2d893218ec95c343cde1786b935952d
unknown <doan80040@gmail.com>
2025-03-12 07:55:43 +0700

    K23CNT1-DOANTNGOCANH-REACTJS

---
+++ Day09/dtna-hook/src/DtnaApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DtnaUseState from './components/DtnaUseState'
+
+export default function DtnaApp() {
+  return (
+    <div className='container border mt-3'>
+      <h1> React Hook</h1>
+      <hr/>
+      <DtnaUseState />
+      </div>
+  )
+}
